cf896409b970387c1907031529958b44ed75f7c6
Chris LoJacono <clojacono92@gmail.com>
2021-01-28 20:03:18 -0600

    phase 2 complete

--- The Heist/Program.cs
+++ The Heist/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace The_Heist
 {
@@ -6,21 +7,50 @@ namespace The_Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Plan Your Heist!");
-            Console.WriteLine("Enter a Team Members Name");
-            var teamName = Console.ReadLine();
+            var yourTeam = new List<TeamMember> { };
+            bool askForUserInput = true;
+            while (askForUserInput)
+            {
+                Console.WriteLine("Plan Your Heist!");
+                Console.WriteLine("Enter a Team Members Name");
+                var teamName = Console.ReadLine();
+                if (String.IsNullOrEmpty(teamName))
+                {
+                    askForUserInput = false;
+                    break;
+                }
 
-            Console.WriteLine("Enter The Team members skill level 0-100");
-            var skillLevel = Console.ReadLine();
-            var convertedSkillLevel = int.Parse(skillLevel);
+                Console.WriteLine("Enter The Team members skill level 0-100");
+                var skillLevel = Console.ReadLine();
+                if (String.IsNullOrEmpty(skillLevel))
+                {
+                    askForUserInput = false;
+                    break;
+                }
+                var convertedSkillLevel = int.Parse(skillLevel);
 
-            Console.WriteLine("Enter the team members courage level 0.0 - 2.0");
-            var courageLevel = Console.ReadLine();
-            var convertedCourageLevel = double.Parse(courageLevel);
+                Console.WriteLine("Enter the team members courage level 0.0 - 2.0");
+                var courageLevel = Console.ReadLine();
+                if (String.IsNullOrEmpty(courageLevel))
+                {
+                    askForUserInput = false;
+                    break;
+                }
+                var convertedCourageLevel = double.Parse(courageLevel);
 
-            var firstTeamMember = new TeamMember(teamName, convertedSkillLevel, convertedCourageLevel);
+                var firstTeamMember = new TeamMember(teamName, convertedSkillLevel, convertedCourageLevel);
+                yourTeam.Add(firstTeamMember);
 
-            firstTeamMember.ShowInfo();
+                firstTeamMember.ShowInfo();
+            }
+            
+            Console.WriteLine($"There a {yourTeam.Count} skilled members of your team!");
+            foreach (var member in yourTeam)
+            {
+                Console.WriteLine($"You have {member.Name} with a skill level of {member.SkillLevel} and a courage of {member.Courage}");
+
+            }
+
         }
     }
 }
