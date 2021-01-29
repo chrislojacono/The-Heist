8b20d65dcaf30d5a7752c8cdf4385024a857a28a
Chris LoJacono <clojacono92@gmail.com>
2021-01-28 19:20:12 -0600

    first phase complete

---
+++ The Heist/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace The_Heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine("Enter a Team Members Name");
+            var teamName = Console.ReadLine();
+
+            Console.WriteLine("Enter The Team members skill level 0-100");
+            var skillLevel = Console.ReadLine();
+            var convertedSkillLevel = int.Parse(skillLevel);
+
+            Console.WriteLine("Enter the team members courage level 0.0 - 2.0");
+            var courageLevel = Console.ReadLine();
+            var convertedCourageLevel = double.Parse(courageLevel);
+
+            var firstTeamMember = new TeamMember(teamName, convertedSkillLevel, convertedCourageLevel);
+
+            firstTeamMember.ShowInfo();
+        }
+    }
+}
---
+++ The Heist/TeamMember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace The_Heist
+{
+    class TeamMember
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public double Courage { get; set; }
+
+        public TeamMember(string name, int skillLevel, double courage)
+        {
+            Name = name;
+            SkillLevel = skillLevel;
+            Courage = courage;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Your First team member {Name} has a skill level of {SkillLevel} and a courage level of {Courage}");
+        }
+    }
+}
